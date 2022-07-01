eae3ac77ecd1b6c6bc53e478ae7362b679165fb6
Daniel <10912932+b4u7@users.noreply.github.com>
2022-07-01 17:23:40 +0100

    feat: replaced Framer Motion with React Transition Group

--- src/components/Bubble.tsx
+++ src/components/Bubble.tsx
@@ -1,4 +1,3 @@
-import { motion } from 'framer-motion'
 import { forwardRef, ReactElement } from 'react'
 import '../styles/bubble.css'
 
@@ -10,12 +9,12 @@ type content = {
 const Bubble = forwardRef<HTMLDivElement, content>((props, ref) => (
   <div
     ref={ref}
-    className="bubble bg-white relative w-fit text-slate-900 p-4 rounded-3xl dark:bg-slate-500 dark:text-slate-100"
+    className={`${
+      props.hidden ? 'opacity-0' : 'opacity-100'
+    }  transition-all transition-200 bubble bg-white relative w-fit text-slate-900 p-4 rounded-3xl dark:bg-slate-500 dark:text-slate-100`}
   >
     {props.children}
   </div>
 ))
 
-const MotionBubble = motion(Bubble, { forwardMotionProps: true })
-
-export default MotionBubble
+export default Bubble
